1f33cef3175c451b4a6d6ebcda4bb776fb868ad1
tarunsai42 <58729635+tarunsai42@users.noreply.github.com>
2024-07-14 14:24:55 -0400

    updated vnet code

--- s1_resourcegroups/resourcegroup-outputs.tf
+++ s1_resourcegroups/resourcegroup-outputs.tf
@@ -2,3 +2,8 @@ output "resource group name" {
   description = "Name of the resource group"
   value = azurerm_resource_group.rg.name
 }
+
+output "rg location" {
+    description = "value of the location Resource group"
+    value = azurerm_resource_group.rg.location
+}
--- s2_networking/virtual-network.tf
+++ s2_networking/virtual-network.tf
@@ -1,13 +1,14 @@
 # Create Virtual Network
-data "azurerm_resource_group" "rg" {
-  name = "rg-hr-dev-tqrpzb"
-}
+# data "azurerm_resource_group" "rg" {
+#   name = "rg-hr-dev-tqrpzb"
+#   location= 
+# }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${local.resource_name_prefix}-${var.vnet_name}"
   address_space       = var.vnet_address_space
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.outputs.location
+  resource_group_name = data.azurerm_resource_group.rg.outputs.rg_name
   tags = local.common_tags
 }
 
