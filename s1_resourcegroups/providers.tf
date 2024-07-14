7c970fa716558af3763d1aab29d3812ed9082fd2
Bhargavmandava <smand9288@gmail.com>
2024-07-14 00:08:37 -0400

    Update workflow to test

--- s1_resourcegroups/providers.tf
+++ s1_resourcegroups/providers.tf
@@ -3,13 +3,25 @@ terraform {
     azurerm = "=3.100.0"
   }
   backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "Terraform-IACS"
+    hostname     = var.terraform_hostname
+    organization = var.terraform_organization
     workspaces {
-      name = "IACS"
+      name = var.terraform_workspace
     }
   }
 }
+# terraform {
+#   required_providers {
+#     azurerm = "=3.100.0"
+#   }
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "Terraform-IACS"
+#     workspaces {
+#       name = "IACS"
+#     }
+#   }
+# }
 
 provider "azurerm" {
   features {}
