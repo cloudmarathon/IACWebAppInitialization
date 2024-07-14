b4aa6e86fffde7a147099b73f73b78f8b4dde44c
Bhargavmandava <smand9288@gmail.com>
2024-07-14 08:47:31 -0400

    test terraform init

--- s1_resourcegroups/providers.tf
+++ s1_resourcegroups/providers.tf
@@ -26,13 +26,13 @@ terraform {
   required_providers {
     azurerm = "=3.100.0"
   }
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "webappknight"
-    workspaces {
-      name = "IAC-webapp-test"
-    }
-  }
+  # backend "remote" {
+  #   hostname     = "app.terraform.io"
+  #   organization = "Terraform-IACS"
+  #   workspaces {
+  #     name = "IAC-webapp-test"
+  #   }
+  # }
 }
 
 provider "azurerm" {
