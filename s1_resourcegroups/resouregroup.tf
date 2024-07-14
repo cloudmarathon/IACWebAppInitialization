78fffa9996a57466faf0d85f1175d9589170b815
tarunsai42 <58729635+tarunsai42@users.noreply.github.com>
2024-07-14 11:11:03 -0400

    updated resource group code

--- s1_resourcegroups/resouregroup.tf
+++ s1_resourcegroups/resouregroup.tf
@@ -1,5 +1,11 @@
-resource "azurerm_resource_group" "webapprg" {
-  name     = var.rg_name
-  tags     = var.tags
-  location = var.location
+# resource "azurerm_resource_group" "webapprg" {
+#   name     = var.rg_name
+#   tags     = var.tags
+#   location = var.location
+# }
+# Resource-1: Azure Resource Group
+resource "azurerm_resource_group" "rg" {
+  name = "${var.resource_group_name}-${local.resource_name_prefix}-${random_string.myrandom.id}"
+  location = var.resource_group_location
+  tags = local.common_tags
 }
--- s1_resourcegroups/terraform.tfvars
+++ s1_resourcegroups/terraform.tfvars
@@ -1,2 +1,4 @@
-rg_name         = "rg-webapp-eastus"
-location        = "eastus"
+business_divsion = "hr"
+environment = "dev"
+resource_group_name = "rg"
+resource_group_location = "eastus"
