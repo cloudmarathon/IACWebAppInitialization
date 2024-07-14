9b8957e9caed50f74a2b4810dd0d03eca401f45e
bhushanmandava <162395532+bhushanmandava@users.noreply.github.com>
2024-07-14 15:17:59 -0400

    feat:update rg

--- s1_resourcegroups/resourcegroup-outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource group name" {
-  description = "Name of the resource group"
-  value = azurerm_resource_group.rg.name
-}
-
-output "rg location" {
-    description = "value of the location Resource group"
-    value = azurerm_resource_group.rg.location
-}
